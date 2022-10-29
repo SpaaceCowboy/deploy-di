0151d810f3ac598f02107f411aaccd52fadaa9ec
spaaceCowboy <shayan.shoeibzadeh69@gmail.com>
2022-10-29 18:44:15 +0300

    laptop to pc test

--- pages/components/Clients.js
+++ pages/components/Clients.js
@@ -7,7 +7,7 @@ export default function Clients() {
        className="flex justify-center items-center flex-wrap w-full ">
         {clients.map((clients) => (
           <div key={clients.id} className="flex-1 flex justify-center items-center 
-          sm:min-w-[192px] min-w-[120px] ">
+          sm:min-w-[1 92px] min-w-[120px] ">
             <img src="{clients.logo}" alt="" 
             className="sm:w-[192px] w-[100px] object-contain "/>
           </div>
