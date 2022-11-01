1985f0556135267bd52c63d153dfc053054cfc26
spaaceCowboy <shayan.shoeibzadeh69@gmail.com>
2022-11-01 16:59:31 +0300

    deploy changes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "583abcf26a91720e"; }
+/******/ 		__webpack_require__.h = function() { return "2aabb26ea8591752"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
